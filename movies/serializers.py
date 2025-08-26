26f415fc1d172775af4e48784f3942ba38fc9298
maxim <maximchukavovmc@gmail.com>
2025-08-26 17:34:33 +0300

    add GenreSerializer and validate_age_restriction method for MovieSerializer

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
 
-from movies.models import Movie
+from movies.models import Movie, Genre
 
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
+        fields = '__all__'
+
+    def validate_age_restriction(self, value):
+        if value < 0 or value > 18:
+            raise serializers.ValidationError("please specify age restriction from 0 to 18")
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
         fields = '__all__'
