4bf21746e33b2def4383e19d35db8b7dd98565e3
maxim <maximchukavovmc@gmail.com>
2025-09-06 17:47:49 +0300

    create IsOwnerOrAdmin permission

--- movies/permissions.py
+++ movies/permissions.py
@@ -7,3 +7,10 @@ class IsSuperUserOrReadOnly(permissions.BasePermission):
 
         return bool(request.user and request.user.is_superuser)
 
+
+class IsOwnerOrAdmin(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user == request.user or request.user.is_staff or request.user.is_superuser
