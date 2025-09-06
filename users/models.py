ae62d55f927fea8ccc8ab5aed6b2ab9c91a00263
maxim <maximchukavovmc@gmail.com>
2025-09-06 13:16:32 +0300

    update class, now class User inherits from AbstractUser

--- users/models.py
+++ users/models.py
@@ -1,13 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(models.Model):
-    name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255, blank=True)
-    user_name = models.CharField(max_length=255, unique=True)
-    email = models.EmailField(max_length=255, unique=True)
-    hashed_password = models.TextField()
-    registered_at = models.DateTimeField(auto_now_add=True)
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
 
     def __str__(self):
-        return f"{self.name} {self.last_name} | {self.user_name} | {self.email} | {self.registered_at}"
+        return self.username
