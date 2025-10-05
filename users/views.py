c1317edf57409ef30c9ebbd66209ad0b7393006e
maxim <maximchukavovmc@gmail.com>
2025-10-05 16:19:26 +0300

    add UserRegister, UsersViewSet

--- users/views.py
+++ users/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
+from drf_spectacular.utils import extend_schema
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.exceptions import TokenError
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import UserRegisterSerializer, UserSerializer
+from .models import User
+
+
+class UserRegisterView(APIView):
+    @extend_schema(
+        request=UserRegisterSerializer,
+    )
+    def post(self, request):
+        serializer = UserRegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                "message": "user created successfully",
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            }, status=201)
+
+        return Response(serializer.errors, status=400)
+
+
+class UsersViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAdminUser,)
+
+    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['post'], permission_classes=[IsAuthenticated])
+    def logout(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"message": "Logged out successfully"}, status=205)
+        except KeyError:
+            return Response({"error": "Refresh token is required"}, status=400)
+        except TokenError:
+            return Response({"error": "Invalid or expired refresh token"}, status=400)
